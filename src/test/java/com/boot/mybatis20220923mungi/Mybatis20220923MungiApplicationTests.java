6240271625671de9b98ce29ddb2e90280d542212
jjmmkk9 <abcdqwerty0192@gmail.com>
2022-09-24 16:47:55 +0900

    mybatis dtudy project init

---
+++ src/test/java/com/boot/mybatis20220923mungi/Mybatis20220923MungiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.mybatis20220923mungi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis20220923MungiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
